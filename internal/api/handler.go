e12bfb6cd01449cd3ac4e82f5aa8382865a22042
unknown <andrii.usatov94@gmail.com>
2026-02-24 14:44:14 +0200

    Add swagger

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1,20 +1,34 @@
 package api
 
+// @title           Chronflow API
+// @version         1.0
+// @termsOfService  http://swagger.io/terms/
+
+// @host      localhost:80
+// @BasePath  /api/v1
+
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
+	_ "github.com/AndriiUsatov/chronflow/docs"
 	config "github.com/AndriiUsatov/chronflow/internal/config/apicfg"
 	"github.com/AndriiUsatov/chronflow/internal/db"
 	"github.com/AndriiUsatov/chronflow/internal/model"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 const (
-	taskEndpoint     = "/api/v1/task"
-	taskByIDEndpoint = "/api/v1/task/:id"
+	home                = "/"
+	taskEndpoint        = "/api/v1/task"
+	taskByIDEndpoint    = "/api/v1/task/:id"
+	taskSwaggerEndpoint = "/swagger/*any"
+	swaggerUI           = "/swagger/index.html"
 )
 
 type taskAPIHandler struct {
@@ -36,34 +50,93 @@ func (api taskAPIHandler) ListenAndServe(isPanic bool) error {
 }
 
 type taskInput struct {
-	URL     string      `form:"url" binding:"required"`
-	Method  string      `form:"method" binding:"required"`
-	RunAt   time.Time   `form:"runAt" binding:"required" time_format:"2006-01-02 15:04:05"`
-	Headers http.Header `form:"headers"`
-	Body    []byte      `form:"body"`
+	URL     string      `json:"url" binding:"required"`
+	Method  string      `json:"method" binding:"required"`
+	RunAt   time.Time   `json:"runAt" binding:"required"`
+	Headers http.Header `json:"headers"`
+	Body    string      `json:"body"`
 }
 
+type taskCreateResponse struct {
+	TaskId string
+}
+
+type APIError struct {
+	Error string `json:"error"`
+}
+
+type taskView struct {
+	ID uuid.UUID
+
+	URL     string
+	Method  string
+	Headers model.JSONHeader
+	Body    string
+
+	Status       model.TaskStatus
+	RunAt        time.Time
+	RetryCount   int
+	Created      time.Time
+	Updated      time.Time
+	ErrorMessage string
+}
+
+// GetTaskByID godoc
+// @Summary      Show an task
+// @Description  Get task by UUID
+// @Tags         task
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "Task UUID"
+// @Success      200  {object}  taskView
+// @Failure      400  {object}  APIError
+// @Failure      404  {object}  APIError
+// @Failure      500  {object}  APIError
+// @Router       /task/{id} [get]
 func (api taskAPIHandler) getTaskByIDHandler(ctx *gin.Context) {
 	id, err := uuid.Parse(ctx.Param("id"))
 
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusBadRequest, APIError{err.Error()})
 	}
 
 	task, err := api.repo.GetTaskByUUID(ctx.Request.Context(), id)
 
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusBadRequest, APIError{err.Error()})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, task)
+	ctx.JSON(http.StatusOK, taskView{
+		ID:           task.ID,
+		Method:       task.Method,
+		Headers:      task.Headers,
+		Body:         string(task.Body),
+		Status:       task.Status,
+		RunAt:        task.RunAt,
+		RetryCount:   task.RetryCount,
+		Created:      task.Created,
+		Updated:      task.Updated,
+		ErrorMessage: task.ErrorMessage,
+	})
 }
 
+// AddTask godoc
+// @Summary      Add task
+// @Description  Creates task to be processed
+// @Tags         task
+// @Accept       json
+// @Produce      json
+// @Param        task  body  taskInput  true  "Task details"
+// @Success      200  {object}  taskCreateResponse
+// @Failure      400  {object}  APIError
+// @Failure      404  {object}  APIError
+// @Failure      500  {object}  APIError
+// @Router       /task/ [post]
 func (api taskAPIHandler) addTaskHandler(ctx *gin.Context) {
 	var tsk taskInput
-	if err := ctx.ShouldBind(&tsk); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	if err := ctx.ShouldBindJSON(&tsk); err != nil {
+		ctx.JSON(http.StatusBadRequest, APIError{err.Error()})
 		return
 	}
 	id := uuid.New()
@@ -73,19 +146,19 @@ func (api taskAPIHandler) addTaskHandler(ctx *gin.Context) {
 		model.Task{
 			ID:      id,
 			URL:     tsk.URL,
-			Method:  tsk.Method,
+			Method:  strings.ToUpper(tsk.Method),
 			RunAt:   tsk.RunAt,
 			Headers: model.JSONHeader(tsk.Headers),
-			Body:    tsk.Body,
+			Body:    []byte(tsk.Body),
 		},
 	)
 
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusInternalServerError, APIError{err.Error()})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"task_id": id.String()})
+	ctx.JSON(http.StatusOK, taskCreateResponse{id.String()})
 }
 
 func GetTaskRestServer(cfg config.ApiConfig, taskRepo db.TaskRepository) taskAPIHandler {
@@ -97,8 +170,12 @@ func GetTaskRestServer(cfg config.ApiConfig, taskRepo db.TaskRepository) taskAPI
 		repo:    taskRepo,
 	}
 
+	router.GET(taskSwaggerEndpoint, ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.GET(taskByIDEndpoint, result.getTaskByIDHandler)
 	router.POST(taskEndpoint, result.addTaskHandler)
+	router.GET(home, func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusSeeOther, swaggerUI)
+	})
 
 	return result
 
